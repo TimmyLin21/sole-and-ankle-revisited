e641c24afbdf6bc86f872b38cd13d04e1dd1d961
TimmyLin21 <f7964032@gmail.com>
2022-08-21 15:25:25 +0800

    Exercise 5
    feature: add fluid desktop navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
-import Icon from '../Icon';
-import UnstyledButton from '../UnstyledButton';
+import { COLORS, WEIGHTS } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
+import MobileMenu from "../MobileMenu";
+import Icon from "../Icon";
+import UnstyledButton from "../UnstyledButton";
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -33,13 +33,13 @@ const Header = () => {
         </Nav>
         <MobileActions>
           <UnstyledButton>
-            <Icon id='shopping-bag'/>
+            <Icon id="shopping-bag" />
           </UnstyledButton>
           <UnstyledButton>
-            <Icon id='search'/>
+            <Icon id="search" />
           </UnstyledButton>
           <UnstyledButton onClick={() => setShowMobileMenu(true)}>
-            <Icon id='menu'/>
+            <Icon id="menu" />
           </UnstyledButton>
         </MobileActions>
         <Fill />
@@ -59,12 +59,14 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
-  @media ${p => p.theme.QUERIES.tabletAndSmaller} {
+  overflow-x: auto;
+  overflow-y: hidden;
+  @media ${(p) => p.theme.QUERIES.tabletAndSmaller} {
     border-top: 4px solid ${COLORS.gray[900]};
-    justify-content:space-between;
+    justify-content: space-between;
     align-items: center;
   }
-  @media ${p => p.theme.QUERIES.phoneAndSmaller} {
+  @media ${(p) => p.theme.QUERIES.phoneAndSmaller} {
     padding-right: 16px;
     padding-left: 16px;
   }
@@ -72,26 +74,27 @@ const MainHeader = styled.div`
 
 const Nav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1.5rem, 9vw - 4rem, 3rem);
   margin: 0px 48px;
-  @media ${p => p.theme.QUERIES.tabletAndSmaller} {
+  margin-right: 0px;
+  @media ${(p) => p.theme.QUERIES.tabletAndSmaller} {
     display: none;
   }
 `;
 
 const LogoWrapper = styled.div`
   flex: 1;
-  @media ${p => p.theme.QUERIES.tabletAndSmaller} {
-    flex:revert;
+  @media ${(p) => p.theme.QUERIES.tabletAndSmaller} {
+    flex: revert;
   }
 `;
 
 const Fill = styled.div`
   flex: 1;
-  @media ${p => p.theme.QUERIES.tabletAndSmaller} {
+  @media ${(p) => p.theme.QUERIES.tabletAndSmaller} {
     display: none;
   }
-`
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
@@ -108,14 +111,14 @@ const NavLink = styled.a`
 const MobileActions = styled.div`
   display: none;
 
-  @media ${p => p.theme.QUERIES.tabletAndSmaller} {
+  @media ${(p) => p.theme.QUERIES.tabletAndSmaller} {
     display: flex;
     gap: 40px;
   }
 
-  @media ${p => p.theme.QUERIES.phoneAndSmaller} {
+  @media ${(p) => p.theme.QUERIES.phoneAndSmaller} {
     gap: 28px;
   }
-`
+`;
 
 export default Header;
